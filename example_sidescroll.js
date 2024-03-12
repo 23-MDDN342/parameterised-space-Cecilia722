8886b8b7bcc8595aa771778e58d53de9bfd71d88
Cecilia722 <114236944+Cecilia722@users.noreply.github.com>
2024-03-12 14:19:03 +1300

    1
    1

--- example_sidescroll.js
+++ example_sidescroll.js
@@ -12,17 +12,19 @@ function draw_one_frame(cur_frac) {
   ellipse(0.25 * width, 0.10 * height, sun_size);
 
   // grass
-  fill("#DDE5B6");
+  fill(0,0,0);
   rect(0, height/2, width, height/2);
 
   stroke(0);
   fill(100, 100, 100);
 
-  let b1_y = 0.55 * height;
-  let b2_y = 0.65 * height;
+  let b1_y = 1/10* height;
+  let b2_y = 0.6* height;
 
-  let b1_size = height/100;
+  let b1_size = height/120;
   let b2_size = height/6;
+
+  let silk_space=height/120
 
 
 
@@ -51,13 +53,13 @@ function draw_one_frame(cur_frac) {
     color_10,
 
     color_1,
-    color_2,
+    color_10,
     color_3,
     color_4,
     color_5,
     color_6,
     color_7,
-    color_8,
+    color_10,
     color_9,
     color_10,
 
@@ -67,11 +69,10 @@ function draw_one_frame(cur_frac) {
     color_4,
     color_5,
     color_6,
-    color_7,
+    color_10,
     color_8,
     color_9,
     color_10,
-
     
   ]
  
@@ -89,6 +90,16 @@ function draw_one_frame(cur_frac) {
     1.2* width,
     1.4* width,
     1.6* width,
+    1.8*width,
+    2.0*width,
+  
+    2.2* width,
+    2.4* width,
+    2.6* width,
+    3.8*width,
+    4.0*width
+
+
    ]
 
   if (debugView) {
@@ -96,67 +107,64 @@ function draw_one_frame(cur_frac) {
  
     strokeWeight(height/100);
     noFill();
-    for(let i=0; i<grid_points1.length; i++) {
-      rect(grid_points1[i], b1_y, b1_size, 3*b1_size);
+    for(let i=0; i<arrayOfColours.length-1; i++) {
+      cur_x_pos = map(cur_frac, 0, 1,grid_points1[i], width*2)
+      rect(cur_x_pos - i*width/10, b1_y,b1_size*20, b1_size); 
     }    
   }
 
   
 
-  fill(color_1);
   noStroke()
-
 
   for(let i=0; i<arrayOfColours.length-1; i++) {
     fill(arrayOfColours[i])
     let cur_x_pos = map(cur_frac, 0, 1,grid_points1[i], width*2)
-    rect(cur_x_pos - i*width/10, b1_y,20*b1_size, 1*b1_size);
+    rect(cur_x_pos - i*width/10, b1_y,b1_size*20, b1_size);
+
+  
+  }
+   
+  noStroke()
+
+  for(let i=0; i<arrayOfColours.length-1; i++) {
+    fill(arrayOfColours[i])
+    let cur_x_pos = map(cur_frac, 1, 0,grid_points1[i], width*2)
+    rect(cur_x_pos - i*width/10, b1_y,b1_size*20, b1_size);
 
   
   }
 
-   
-  // let grid_points3= [
-  //   - 0.8* width,
-  //   -0.6 * width,
-  //    -0.4 * width,
-  //    -0.2 * width,
-  //    -0* width,
-  //    0.2 * width,
-  //    0.4 * width,
-  //    0.6* width,
-  //    0.8 * width,
-  //   1* width,
-  //   1.2* width,
-  //   1.4* width,
-  //   1.6* width,
-  //  ]
-
-  // if (debugView) {
-  //   stroke(250, 0, 0);
- 
-  //   strokeWeight(height/100);
-  //   noFill();
-  //   for(let i=0; i<grid_points3.length; i++) {
-  //     rect(grid_points3[i], b1_y, b1_size, 3*b1_size);
-  //   }    
-  // }
+  
+  for(let i=0; i<arrayOfColours.length-1; i++) {
+    fill(arrayOfColours[i])
+    let cur_x_pos = map(cur_frac, 1, 0,grid_points1[i], width*2)
+    rect(cur_x_pos - i*width/10-width/20, b1_y-silk_space,b1_size*20, b1_size);
 
   
+  }
 
-  // fill(color_1);
-  // noStroke()
+  for(let i=0; i<arrayOfColours.length-1; i++) {
+    fill(arrayOfColours[i])
+    let cur_x_pos = map(cur_frac, 0, 1,grid_points1[i], width*2)
+    rect(cur_x_pos - i*width/10-width/20, b1_y-silk_space,b1_size*20, b1_size);
+  
+  }
 
-
-  // for(let i=0; i<arrayOfColours.length-1; i++) {
-  //   fill(arrayOfColours[i])
-  //   let cur_x_pos = map(cur_frac, 0, 1,grid_points3[i], width*2)
-  //   rect(cur_x_pos - i*width/10, b1_y,20*b1_size, 1*b1_size);
+  for(let i=0; i<arrayOfColours.length-1; i++) {
+    fill(arrayOfColours[i])
+    let cur_x_pos = map(cur_frac, 1, 0,grid_points1[i], width*2)
+    rect(cur_x_pos - i*width/10-width/20, b1_y+silk_space,b1_size*20, b1_size);
 
   
+  }
 
-
-
+  for(let i=0; i<arrayOfColours.length-1; i++) {
+    fill(arrayOfColours[i])
+    let cur_x_pos = map(cur_frac, 0, 1,grid_points1[i], width*2)
+    rect(cur_x_pos - i*width/10-width/20, b1_y+silk_space,b1_size*20, b1_size);
+  
+  }
 
 
 
